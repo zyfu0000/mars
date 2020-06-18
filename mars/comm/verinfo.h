03a0992a9bef6d43c2362c0e11d1524e2d3db95d
alexqian <alexqian@tencent.com>
2020-06-18 10:19:57 +0800

    多文件编译脚本修改

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "b38134a8"
-#define MARS_PATH "master"
+#define MARS_REVISION "4530c9a5"
+#define MARS_PATH "multi_file_tmp"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-04-26 16:39:50"
+#define MARS_BUILD_TIME "2020-06-18 10:12:33"
 #define MARS_TAG ""
 
 #endif
