f3ffca848d81a64004fda9af5af811c3036c5d85
zhiyangfu <zhiyangfu@tencent.com>
2020-06-18 10:22:50 +0800

    Revert "patch from alex"
    This reverts commit 338477ae0c563caae2488361c58272b650ff9239.

--- mars/mars_utils.py
+++ mars/mars_utils.py
@@ -6,68 +6,65 @@ import subprocess
 
 
 COMM_COPY_HEADER_FILES = {
-    "mars/comm/verinfo.h": "comm",
-    "mars/comm/autobuffer.h": "comm",
-    "mars/comm/http.h": "comm",
-    "mars/comm/time_utils.h": "comm",
-    "mars/comm/strutil.h": "comm",
-    "mars/comm/string_cast.h": "comm",
-    "mars/comm/comm_data.h": "comm",
-    "mars/comm/projdef.h": "comm",
-    "mars/comm/platform_comm.h": "comm",
-    "mars/comm/socket/local_ipstack.h": "comm",
-    "mars/comm/socket/nat64_prefix_util.h": "comm",
-    "mars/comm/has_member.h": "comm",
-    "mars/comm/objc/scope_autoreleasepool.h": "comm",
-    "mars/comm/objc/ThreadOperationQueue.h": "comm",
-    "mars/stn/stn.h": "stn",
-    "mars/stn/stn_logic.h": "stn",
-    "mars/stn/proto/stnproto_logic.h": "stn",
-    "mars/baseevent/base_logic.h": "baseevent",
-    "mars/comm/xlogger/preprocessor.h": "xlog",
-    "mars/comm/xlogger/xloggerbase.h": "xlog",
-    "mars/comm/xlogger/xlogger.h": "xlog",
-    "mars/log/appender.h": "xlog",
-    "mars/log/xlogger_interface.h": "xlog",
-    "mars/app/app.h": "app",
-    "mars/app/app_logic.h": "app",
-    "mars/sdt/sdt.h": "sdt",
-    "mars/sdt/sdt_logic.h": "sdt",
-    "mars/sdt/constants.h": "sdt",
-    "mars/sdt/netchecker_profile.h": "sdt",
-    "mars/stn/proto/longlink_packer.h": "stn/proto",
-}
+            "mars/comm/verinfo.h": "comm",
+            "mars/comm/autobuffer.h": "comm",
+            "mars/comm/http.h": "comm",
+            "mars/comm/time_utils.h": "comm",
+            "mars/comm/strutil.h": "comm",
+            "mars/comm/string_cast.h": "comm",
+            "mars/comm/comm_data.h": "comm",
+            "mars/comm/projdef.h": "comm",
+            "mars/comm/platform_comm.h": "comm",
+            "mars/comm/socket/local_ipstack.h": "comm",
+            "mars/comm/socket/nat64_prefix_util.h": "comm",
+            "mars/comm/has_member.h" : "comm",
+            "mars/comm/objc/scope_autoreleasepool.h": "comm",
+            "mars/comm/objc/ThreadOperationQueue.h": "comm",
+            "mars/stn/stn.h": "stn",
+            "mars/stn/stn_logic.h": "stn",
+            "mars/stn/proto/stnproto_logic.h": "stn",
+            "mars/baseevent/base_logic.h": "baseevent",
+            "mars/comm/xlogger/preprocessor.h": "xlog",
+            "mars/comm/xlogger/xloggerbase.h": "xlog",
+            "mars/comm/xlogger/xlogger.h": "xlog",
+            "mars/log/appender.h": "xlog",
+            "mars/app/app.h": "app",
+            "mars/app/app_logic.h": "app",
+            "mars/sdt/sdt.h": "sdt",
+            "mars/sdt/sdt_logic.h": "sdt",
+            "mars/sdt/constants.h": "sdt",
+            "mars/sdt/netchecker_profile.h": "sdt",
+            "mars/stn/proto/longlink_packer.h": "stn/proto",
+            }        
 
 
 WIN_COPY_EXT_FILES = {
-    "mars/comm/platform_comm.h": "comm",
-    "mars/comm/windows/projdef.h": "comm/windows",
-    "mars/comm/windows/sys/cdefs.h": "comm/windows/sys",
-    "mars/comm/windows/sys/time.h": "comm/windows/sys",
-    "mars/comm/platform_comm.h": "comm",
+            "mars/comm/platform_comm.h": "comm",
+            "mars/comm/windows/projdef.h": "comm/windows",
+            "mars/comm/windows/sys/cdefs.h": "comm/windows/sys",
+            "mars/comm/windows/sys/time.h": "comm/windows/sys",
+            "mars/comm/platform_comm.h": "comm",
 }
 
 XLOG_COPY_HEADER_FILES = {
-    "mars/comm/verinfo.h": "comm",
-    "mars/comm/autobuffer.h": "comm",
-    "mars/comm/http.h": "comm",
-    "mars/comm/time_utils.h": "comm",
-    "mars/comm/strutil.h": "comm",
-    "mars/comm/string_cast.h": "comm",
-    "mars/comm/comm_data.h": "comm",
-    "mars/comm/projdef.h": "comm",
-    "mars/comm/socket/local_ipstack.h": "comm",
-    "mars/comm/socket/nat64_prefix_util.h": "comm",
-    "mars/comm/has_member.h": "comm",
-    "mars/comm/objc/scope_autoreleasepool.h": "comm",
-    "mars/comm/objc/ThreadOperationQueue.h": "comm",
-    "mars/comm/xlogger/preprocessor.h": "xlog",
-    "mars/comm/xlogger/xloggerbase.h": "xlog",
-    "mars/comm/xlogger/xlogger.h": "xlog",
-    "mars/log/appender.h": "xlog",
-    "mars/log/xlogger_interface.h": "xlog",
-}
-
+            "mars/comm/verinfo.h": "comm",
+            "mars/comm/autobuffer.h": "comm",
+            "mars/comm/http.h": "comm",
+            "mars/comm/time_utils.h": "comm",
+            "mars/comm/strutil.h": "comm",
+            "mars/comm/string_cast.h": "comm",
+            "mars/comm/comm_data.h": "comm",
+            "mars/comm/projdef.h": "comm",
+            "mars/comm/socket/local_ipstack.h": "comm",
+            "mars/comm/socket/nat64_prefix_util.h": "comm",
+            "mars/comm/has_member.h" : "comm",
+            "mars/comm/objc/scope_autoreleasepool.h": "comm",
+            "mars/comm/objc/ThreadOperationQueue.h": "comm",
+            "mars/comm/xlogger/preprocessor.h": "xlog",
+            "mars/comm/xlogger/xloggerbase.h": "xlog",
+            "mars/comm/xlogger/xlogger.h": "xlog",
+            "mars/log/appender.h": "xlog",
+            }      
 
 class bcolors:
     HEADER = '\033[95m'
@@ -78,31 +75,29 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
-
+    
 def libtool_libs(src_libs, dst_lib):
     src_lib_str = ''
     for l in src_libs:
-        src_lib_str = '%s %s' % (src_lib_str, l)
+        src_lib_str = '%s %s'%(src_lib_str, l)
 
     print(src_lib_str)
-    ret = os.system('libtool -static -no_warning_for_no_symbols -o %s %s' % (dst_lib, src_lib_str))
+    ret = os.system('libtool -static -no_warning_for_no_symbols -o %s %s' %(dst_lib, src_lib_str))
     if ret != 0:
-        print('!!!!!!!!!!!libtool %s fail!!!!!!!!!!!!!!!' % (dst_lib))
+        print('!!!!!!!!!!!libtool %s fail!!!!!!!!!!!!!!!' %(dst_lib))
         return False
 
     return True
 
-
 def lipo_libs(src_libs, dst_lib):
     src_lib_str = u''
     for l in src_libs:
-        src_lib_str = '%s %s' % (src_lib_str, l)
+        src_lib_str = '%s %s'%(src_lib_str, l)
 
-    cmd = 'lipo -create %s -output %s' % (src_lib_str, dst_lib)
+    cmd = 'lipo -create %s -output %s' %(src_lib_str, dst_lib)
     ret = os.system(cmd)
     if ret != 0:
-        print('!!!!!!!!!!!lipo_libs %s fail, cmd:%s!!!!!!!!!!!!!!!' % (dst_lib, cmd))
+        print('!!!!!!!!!!!lipo_libs %s fail, cmd:%s!!!!!!!!!!!!!!!' %(dst_lib, cmd))
         return False
 
     return True
@@ -116,10 +111,10 @@ def lipo_thin_libs(src_lib, dst_lib, archs):
         else:
             tmp_result = dst_lib + '.' + arch
 
-        cmd = 'lipo %s -thin %s -output %s' % (src_lib, arch, tmp_result)
+        cmd = 'lipo %s -thin %s -output %s' %(src_lib, arch, tmp_result)
         ret = os.system(cmd)
         if ret != 0:
-            print('!!!!!!!!!!!lipo_thin_libs %s fail, cmd:%s!!!!!!!!!!!!!!!' % (tmp_result, cmd))
+            print('!!!!!!!!!!!lipo_thin_libs %s fail, cmd:%s!!!!!!!!!!!!!!!' %(tmp_result, cmd))
             return False
         tmp_results.append(tmp_result)
 
@@ -128,13 +123,9 @@ def lipo_thin_libs(src_lib, dst_lib, archs):
     else:
         return lipo_libs(tmp_results, dst_lib)
 
-
 GENERATE_DSYM_FILE_CMD = 'dsymutil %s -o %s'
-
-
 def gen_dwarf_with_dsym(src_dylib, dst_dsym):
-    os.system(GENERATE_DSYM_FILE_CMD % (src_dylib, dst_dsym))
-
+    os.system(GENERATE_DSYM_FILE_CMD %(src_dylib, dst_dsym))
 
 def remove_cmake_files(path):
     cmake_files = path + '/CMakeFiles'
@@ -154,7 +145,6 @@ def remove_cmake_files(path):
     for f in glob.glob(path + '/*.so'):
         os.remove(f)
 
-
 def clean_except(path, except_list):
     for fpath, dirs, fs in os.walk(path):
         in_except = False
@@ -166,7 +156,7 @@ def clean_except(path, except_list):
             remove_cmake_files(fpath)
 
     if not os.path.exists(path):
-        os.makedirs(path)
+        os.makedirs(path)    
 
 
 def clean(path, incremental=False):
@@ -177,15 +167,14 @@ def clean(path, incremental=False):
     if not os.path.exists(path):
         os.makedirs(path)
 
-
 def clean_windows(path, incremental):
     if not os.path.exists(path):
         os.makedirs(path)
         return
-
+    
     if incremental:
-        return
-
+        return;
+    
     try:
         if os.path.exists(path):
             shutil.rmtree(path)
@@ -193,42 +182,40 @@ def clean_windows(path, incremental):
                 os.makedirs(path)
     except Exception:
         pass
-
-
+        
 def copy_windows_pdb(cmake_out, sub_folder, config, dst_folder):
     for sf in sub_folder:
-        src_file = "%s/%s/" % (cmake_out, sf)
+        src_file = "%s/%s/" %(cmake_out, sf)
         dirs = glob.glob(src_file + "*.dir")
         if len(dirs) != 1:
-            print("Warning: %s path error." % src_file)
+            print("Warning: %s path error." %src_file)
             continue
-
-        src_file = "%s/%s" % (dirs[0], config)
+        
+        src_file = "%s/%s" %(dirs[0], config)
         pdbs = glob.glob(src_file + "/*.pdb")
         if len(pdbs) != 1:
-            print("Warning: %s path error." % src_file)
+            print("Warning: %s path error." %src_file)
             continue
         pdb = pdbs[0]
         if os.path.isfile(pdb):
             shutil.copy(pdb, dst_folder)
         else:
-            print("%s not exists" % pdb)
-
+            print("%s not exists" %pdb)
 
 def copy_file(src, dst):
     if not os.path.isfile(src):
-        print('Warning: %s not exist' % (src))
-        return
+        print('Warning: %s not exist' %(src))
+        return;
 
     if dst.rfind("/") != -1 and not os.path.exists(dst[:dst.rfind("/")]):
         os.makedirs(dst[:dst.rfind("/")])
 
     shutil.copy(src, dst)
 
-
 def copy_file_mapping(header_file_mappings, header_files_src_base, header_files_dst_end):
     for (src, dst) in header_file_mappings.items():
         copy_file(header_files_src_base + src, header_files_dst_end + "/" + dst + '/' + src[src.rfind("/"):])
+
 
 
 def make_static_framework(src_lib, dst_framework, header_file_mappings, header_files_src_base='./'):
@@ -238,7 +225,7 @@ def make_static_framework(src_lib, dst_framework, header_file_mappings, header_f
     os.makedirs(dst_framework)
     shutil.copy(src_lib, dst_framework)
 
-    framework_path = dst_framework + '/Headers'
+    framework_path = dst_framework + '/Headers'  
     for (src, dst) in header_file_mappings.items():
         copy_file(header_files_src_base + src, framework_path + "/" + dst + '/' + src[src.rfind("/"):])
 
@@ -251,9 +238,9 @@ def check_ndk_env():
     except KeyError as identifier:
         print("Error: ndk does not exist or you do not set it into NDK_ROOT.")
         return False
-
+    
     if ndk_path is not None and ndk_path.strip():
-        print("ndk path:%s" % ndk_path)
+        print("ndk path:%s"%ndk_path)
 
     if not ndk_path:
         print("Error: ndk does not exist or you do not set it into NDK_ROOT.")
@@ -264,7 +251,7 @@ def check_ndk_env():
         return False
 
     ndk_revision = None
-
+    
     f = open(os.path.join(ndk_path, "source.properties"))
     line = f.readline()
     while line:
@@ -283,7 +270,6 @@ def check_ndk_env():
 
     print("Error: make sure ndk's version >= r16b")
     return False
-
 
 html_css = '''
 <style type="text/css">
@@ -338,7 +324,6 @@ def parse_as_git(path):
 
     return revision, path, url
 
-
 def gen_mars_revision_file(version_file_path, tag=''):
     revision, path, url = parse_as_git(version_file_path)
 
@@ -370,8 +355,8 @@ def gen_mars_revision_file(version_file_path, tag=''):
     }
 
     output = '[[==BUILD_DESCRIPTION==]]Revision: %s %s' % (
-        version_data['PublicComponent']['Revision'],
-        '&nbsp;' * 18)
+    version_data['PublicComponent']['Revision'],
+    '&nbsp;' * 18)
     html = html_css
     data_type = 'PublicComponent'
     html_rows = ''
@@ -382,31 +367,31 @@ def gen_mars_revision_file(version_file_path, tag=''):
 
     output += html
 
-    print(''.join(output.splitlines()))
+    print (''.join(output.splitlines()))
 
-
+    
 def check_vs_env():
     vs_tool_dir = os.getenv("VS140COMNTOOLS")
-
+    
     if not vs_tool_dir:
         print("You must install visual studio 2015 for build.")
         return False
-
+    
     return True
-
+    
 def merge_win_static_libs(src_libs, dst_lib):
-
+    
     vs_tool_dir = os.getenv("VS140COMNTOOLS")
     lib_cmd = vs_tool_dir + '/../../VC/bin/lib.exe'
     print('lib cmd:' + lib_cmd)
-
+    
     src_libs.insert(0, '/OUT:' + dst_lib)
     src_libs.insert(0, lib_cmd)
-
+    
     p = subprocess.Popen(src_libs)
     p.wait()
     if p.returncode != 0:
-        print('!!!!!!!!!!!lib.exe %s fail!!!!!!!!!!!!!!!' % (dst_lib))
+        print('!!!!!!!!!!!lib.exe %s fail!!!!!!!!!!!!!!!' %(dst_lib))
         return False
 
     return True
@@ -414,5 +399,4 @@ def merge_win_static_libs(src_libs, dst_lib):
 
 import glob
 if __name__ == '__main__':
-    lipo_thin_libs(u'/Users/garry/Documents/gitcode/mmnet/mars/openssl/openssl_lib_iOS/libcrypto.a',
-                   u'/Users/garry/Documents/gitcode/mmnet/mars/openssl/openssl_lib_iOS/libcrypto_test.a', ['x86_64', 'arm64'])
+    lipo_thin_libs(u'/Users/garry/Documents/gitcode/mmnet/mars/openssl/openssl_lib_iOS/libcrypto.a', u'/Users/garry/Documents/gitcode/mmnet/mars/openssl/openssl_lib_iOS/libcrypto_test.a', ['x86_64', 'arm64'])
